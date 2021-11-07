b2252bc21da7710701c68bf47778705185ec96e4
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-07 19:48:03 +0100

    chore: add store ids

--- release.config.js
+++ release.config.js
@@ -11,13 +11,13 @@ module.exports = {
       prepareCmd: 'yarn build:prepare ${nextRelease.version} && yarn build',
     }],
     // ['semantic-release-firefox-add-on', {
-    //   extensionId: '',
+    //   extensionId: '{e8af7e2f-990a-4b24-8a1b-83a923b2f2ac}',
     //   targetXpi: 'wikipedia-plus--extension-${nextRelease.version}.xpi',
     //   artifactsDir: 'packages',
     //   channel: 'listed',
     // }],
     // ['semantic-release-chrome', {
-    //   extensionId: '',
+    //   extensionId: 'kdajpcjipejndikbjabigodoeaaoopkg',
     //   asset: 'wikipedia-plus--extension-${nextRelease.version}.zip',
     // }],
     ['@semantic-release/github', {
